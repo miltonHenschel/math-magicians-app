cbaeba19298b8a2ede57a31301d07649e0bd8f28
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-03-09 05:08:32 +0100

    Add NavLinks using ReactRoutes

---
+++ src/routes/Quotes.js
@@ -0,0 +1,28 @@
+import '../styles/quotes.css';
+
+const Quotes = () => (
+  <>
+    <div className="padding">
+      <h2 className="notBold">
+        Mathematics as an expression of the human mind reflects the active will,
+        the contemplative reason, and the desire for aesthetic perfection. Its
+        basic elements are logic and intuition, analysis and construction,
+        generality and individuality.
+        <br />
+        <span className="bold">
+          — Richard Courant, German-American mathematician
+        </span>
+      </h2>
+      <h2 className="notBold">
+        Mathematics are the result of mysterious powers which no one
+        understands, and which the unconscious recognition of beauty must play
+        an important part. Out of an infinity of designs a mathematician chooses
+        one pattern for beauty’s sake and pulls it down to earth.
+        <br />
+        <span className="bold">— Marston Morse, American mathematician</span>
+      </h2>
+    </div>
+  </>
+);
+
+export default Quotes;
